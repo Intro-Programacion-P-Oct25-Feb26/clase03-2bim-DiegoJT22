94576c3b42d592e7411b10779da20b2bd32e2fd1
DiegoJT22 <daigoestrlar22@gmail.com>
2025-12-17 12:39:40 -0500

    Solución paquete05 Ejercicio

--- Ejemplosc3/src/paquete05/Ejercicio.java
+++ Ejemplosc3/src/paquete05/Ejercicio.java
@@ -4,12 +4,21 @@
  */
 package paquete05;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author daigo
  */
 public class Ejercicio {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        
+        int[][] arreglo1 = new int[4][11];
+        String[] nombresEstudiante = new String[5];
+        double sumaIngresos;
         
     }
     
